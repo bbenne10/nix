9f2bdb48f13d4e8bba959247c8ca9dc9ea713acd
Bryan Bennett <bbenne10@gmail.com>
2021-04-07 11:06:32 -0400

    Add macos specific path for linking apps into ~/Applications

---
+++ lib/macos.nix
@@ -0,0 +1,29 @@
+{config, pkgs, lib, ... }:
+{
+  imports = [
+    ./common.nix
+    # ./homebrew.nix
+  ];
+
+  home.activation = {
+    copyApplications = let
+      apps = pkgs.buildEnv {
+        name = "home-manager-applications";
+        paths = config.home.packages;
+        pathsToLink = "/Applications";
+      };
+    in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      baseDir="$HOME/Applications/Home Manager Apps"
+      if [ -d "$baseDir" ]; then
+        rm -rf "$baseDir"
+      fi
+      mkdir -p "$baseDir"
+      for appFile in ${apps}/Applications/*; do
+        target="$baseDir/$(basename "$appFile")"
+        echo "=> $appFile -> $target"
+        $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+        $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+      done
+    '';
+  };
+}
