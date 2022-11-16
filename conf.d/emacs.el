7322887806990b9022ce9980fd02f3ef240a418a
Bryan Bennett <Bryan.Bennett@protonmail.com>
2022-11-16 15:00:02 -0500

    Emacs: Don't poll git so frequently

--- conf.d/emacs.el
+++ conf.d/emacs.el
@@ -145,7 +145,7 @@
 (use-package git-gutter
   :hook (prog-mode . git-gutter-mode)
   :config
-  (setq git-gutter:update-interval 0.02))
+  (setq git-gutter:update-interval 2))
 
 (use-package git-gutter-fringe
   :config
