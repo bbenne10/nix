c3e0dc0b104e79d234fbd9cabe65016b6c48acc3
Bryan Bennett <Bryan.Bennett@protonmail.com>
2022-12-24 10:21:15 -0500

    Replace firefox with qutebrowser on linux

--- lib/linux.nix
+++ lib/linux.nix
@@ -2,11 +2,11 @@
   home-manager.users.${userName} = {
     home.packages = with pkgs; [
       brightnessctl
-      firefox
       foot
       kanshi
       pamixer
       playerctl
+      qutebrowser
       river
       spotify
       wofi
@@ -16,5 +16,6 @@
     home.file.".config/foot/foot.ini".source = ./../conf.d/foot.ini;
     home.file.".config/yambar/config.yml".source = ./../conf.d/yambar.yml;
     home.file.".config/kanshi/config".source = ./../conf.d/kanshi_config;
+    home.file.".config/qutebrowser/autoconfig.yml".source = ./../conf.d/qutebrowser_autoconfig.yml;
   };
 }
