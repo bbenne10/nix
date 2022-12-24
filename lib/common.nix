334c0838f92e8d3fd99715c622dee4893732ff55
Bryan Bennett <Bryan.Bennett@protonmail.com>
2022-12-24 10:24:44 -0500

    Move ssh authorizedKeys to linux specific config

--- lib/common.nix
+++ lib/common.nix
@@ -27,10 +27,6 @@ in {
 
   users.users.${userName} = {
     home = homePath;
-    # TODO: Fix this? I guess?
-    # openssh.authorizedKeys.keys = [
-    #   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQtmdA5vhoNcN14PeFS80Y++BVPSBJKajg1hlqdr5dwhr+Ug6zvUHVpJy36FZvM6VL0t/cB4GwFpv9B+tHkECTfHQgQLvQ1pQIua5ByEf3hhc5owVWA3WOQa9E92F+PFR/AjNJHaQqSAZevYobxRT03r4fCkwaODXWuttz0314hV0HJMZPXZQxHrPEpBBmm7AcetWsu4zExCwwEODK1aT7WvDUp6CvIQaAqRSkfZQhirD//E7XgChTvVcVbjVV2E6akSOPr0cAZb08P6/XjXemddV3ohJtgzGVB8zixCf34Z53etD4j6MaVWjiRmv5J2Pffc7Kzwwdjs+LFkSr328L cardno:000606534762"
-    # ];
   };
 
   fonts = {
--- lib/linux.nix
+++ lib/linux.nix
@@ -18,4 +18,9 @@
     home.file.".config/kanshi/config".source = ./../conf.d/kanshi_config;
     home.file.".config/qutebrowser/autoconfig.yml".source = ./../conf.d/qutebrowser_autoconfig.yml;
   };
+  users.users.${userName} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQtmdA5vhoNcN14PeFS80Y++BVPSBJKajg1hlqdr5dwhr+Ug6zvUHVpJy36FZvM6VL0t/cB4GwFpv9B+tHkECTfHQgQLvQ1pQIua5ByEf3hhc5owVWA3WOQa9E92F+PFR/AjNJHaQqSAZevYobxRT03r4fCkwaODXWuttz0314hV0HJMZPXZQxHrPEpBBmm7AcetWsu4zExCwwEODK1aT7WvDUp6CvIQaAqRSkfZQhirD//E7XgChTvVcVbjVV2E6akSOPr0cAZb08P6/XjXemddV3ohJtgzGVB8zixCf34Z53etD4j6MaVWjiRmv5J2Pffc7Kzwwdjs+LFkSr328L cardno:000606534762"
+    ];
+  };
 }
