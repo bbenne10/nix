27ddbfe58841a815c5303861ce4added81acbe5f
Bryan Bennett <Bryan.Bennett@protonmail.com>
2023-07-05 15:27:25 -0400

    Emacs: Update ef-themes
    this gets us ef-elea

--- lib/graphical.nix
+++ lib/graphical.nix
@@ -47,6 +47,13 @@ in
         alwaysEnsure = true;
         override = epkgs: epkgs // {
           inherit bennett-themes;
+          ef-themes = epkgs.ef-themes.overrideAttrs {
+            version = "1.2.0";
+            src = pkgs.fetchurl {
+              url = "https://elpa.gnu.org/packages/ef-themes-1.2.0.tar";
+              sha256 = "0uEZ35u9eA8/DQDyLlv1JWnhq8hvAGakOd2YiTzhk+g=";
+            };
+          };
         };
       };
     };
