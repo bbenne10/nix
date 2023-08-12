76b3fe74d8402678f33cb62661024f814b8bb7a4
Bryan Bennett <Bryan.Bennett@protonmail.com>
2023-08-12 09:08:30 -0400

    Switch to fork of everforest theme

--- derivations/everforest-emacs.nix
+++ derivations/everforest-emacs.nix
@@ -1,15 +1,30 @@
 { emacsPackages, lib, fetchFromGitHub }: emacsPackages.trivialBuild {
   pname = "everforest";
   src = fetchFromGitHub {
-    owner = "Theory-of-Everything";
+    owner = "bbenne10";
     repo = "everforest-emacs";
-    rev = "703b16b742b753f6ad077b5c7f51947d1926c530";
-    sha256 = "3dZ2LMv0esbzJvfrtWWbO9SFotXj3UeizjMxO6vs73M=";
+    rev = "232fb60a61fbb7897a98df5619c0086b83da82ed";
+    sha256 = "fKsjgAg5naN+t6Fx6XxDBlm2Uhy5oc6XavvlfZDbP5w=";
   };
   preBuild = ''
-    ls -lah .
-    false
-    # rm everforest.el
-    # cp editors/emacs-WIP/*.el .
+    rm everforest.el
+    mkdir themes/
+    mv *.el themes/
+    cat << EOF > everforest.el
+        ;;;###autoload
+        (when (and (boundp 'custom-theme-load-path) load-file-name)
+        (let* ((base (file-name-directory load-file-name))
+                (dir (expand-file-name "themes/" base)))
+            (add-to-list 'custom-theme-load-path
+                        (or (and (file-directory-p dir) dir)
+                            base))))
+        (provide 'everforest)
+    EOF
+  '';
+  postBuild = ''
+    emacs -L . --batch -f batch-byte-compile themes/*.el
+  '';
+  postInstall = ''
+    install themes/*.el themes/*.elc $out/share/emacs/site-lisp
   '';
 }
