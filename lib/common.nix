84626edcbcd97df8bb9db5348a96f46d58c49d1c
Bryan Bennett <Bryan.Bennett@protonmail.com>
2024-02-08 08:07:29 -0500

    Swap to new YK5 key for ssh/gpg

--- lib/common.nix
+++ lib/common.nix
@@ -103,7 +103,7 @@ in
       lfs = { enable = true; };
       delta = { enable = true; };
       signing = {
-        key = "41EA00B400F969701CB2D3AFEF90E3E98B8F5C0B";
+        key = "EE149E4215408DE9";
         signByDefault = true;
       };
       ignores = [
@@ -143,43 +143,12 @@ in
       enable = true;
       mutableKeys = false;
       mutableTrust = false;
-      scdaemonSettings = { disable-ccid = true; };
+      scdaemonSettings = {
+        disable-ccid = true;
+      };
       publicKeys = [
         {
-          text = ''
-            -----BEGIN PGP PUBLIC KEY BLOCK-----
-
-            mQENBGG435gBCAC805OTSnHrC7ciKpsy4SDwMrJRCvdqbd6JvkJKrhe09llqcVY7
-            FltPPwetiTXdjDN2+v4AT2LWM9jElRRVi6IOdbQofnHVGO8JDmr6Cd8YugmAn2W6
-            pAqR+1UNF2s6hUqZJNZasIxxrqXeGCFL/eb/t/Fj8aGTKA38ydJMKAB0o93UcbGo
-            x1Mi7wFfNHify+xEUiYcTbtHug0ucwXWSVBT13KnQ1AagXsKu6Af4+V1bdCPvG5q
-            PavY0oRN5mks1B8siwv50VrRuCb96rcNU4HlRmomi2hcZaEKpP2zKYzXjGH5cjJe
-            KpBN1HiCYZ3iF88wBPHEPV7JTx2Jbq0QaooLABEBAAG0LEJyeWFuIEJlbm5ldHQg
-            PEJyeWFuLkJlbm5ldHRAcHJvdG9ubWFpbC5jb20+iQFOBBMBCAA4FiEEQeoAtAD5
-            aXAcstOv75Dj6YuPXAsFAmG435gCGw8FCwkIBwIGFQoJCAsCBBYCAwECHgECF4AA
-            CgkQ75Dj6YuPXAvl3wf8D8ZYjMNyQO1vtM+vubEy9VX9MI8Y+TJUrx7FS7ynpIG5
-            oU4x7mQfW2DpRMG0IALUqF3oVSe1oY5WrZGTnDfjFcmDGGGFm+vgPJQloRc0JTQS
-            7vFVtMZqxX5CRagtLm8JyXU5rO5KGRqrwAHf0+cD+WPt61PEvt39cU+aVHnZ55uv
-            szonV2InWCLzcN5ukMVEptoCDiLyk5PDK59tO364UmkGSeMIleORw4TrauINfmd1
-            6P8iNw9Y4IMgiOc964pVM/gfAEIraZiTN2RNFs44JzKERVHt0dUco/ZCtuceWjs/
-            o4RIRKscIEq5AlJr2Z7zfea8fFNv/zXsgb5LOQjNjLkBDQRhuN/FAQgA0LZnQOb4
-            aDXDdeD3hUvNGPvgVT0gSSmo4NYZana+XcIa/lIOs71B1aSct+hWbzOlS9Lf3AeB
-            sBab/QfrR5BAk3x0IEC70NaUCLmuQchH94YXOaMFVgN1jkGvRPdhfjxUfwIzSR2k
-            KkgGXr2KG8UU9N6+HwpMGjg11rrbc9N9eIVdByTGT12UMR6zxKQQZpuwHHrVrLuM
-            xMQsMBDgytWk+1rw1KegryEGgKkUpH2UIYqw//xO14AoU71XFW41VdhOmpEjj69H
-            AGW9PD+v1413pnXVd6ISbYMxlQfM4sQn9+Ged3rQ+I+jGlVo4kZr+Sdj333Oys8M
-            HY7PixZEq99vCwARAQABiQE2BBgBCAAgFiEEQeoAtAD5aXAcstOv75Dj6YuPXAsF
-            AmG438UCGyAACgkQ75Dj6YuPXAvI/wf/RVjCF9zEw/Xk9rKVG74Mr1MpZbGVT8XQ
-            uXJ0WzZrcfRDkhX5+3+DkF4T4RlvEZKfVsYu5rrjMenGuT9wrX8i0GRxUC8pQ7er
-            aH/gwmh4ohbyqCyz/VisSasq3EeUFlR9OtiIj9BmkI+0aUgOue17ig3LBHduMvu/
-            rlvVkzxFgsRFDcRrZXkOnBX0gJVl7YjY61NBx9Y0AEAfzO34CsuPrf3kt12IaVV8
-            11yglK7c0LtTouKQZxHdky+b5I3JuS28RhfeJ6eWIPc+5p+90Daf8mA92QKMXDvr
-            T4NpJ2/hTmCr49+VOabR0yv1iDjHagZywgT+fTxkwzonAlYXFXyMzw==
-            =1nQI
-            -----END PGP PUBLIC KEY BLOCK-----'';
-          trust = "ultimate";
-        }
-        {
+          # yubikey 5
           text = ''
             -----BEGIN PGP PUBLIC KEY BLOCK-----
 
