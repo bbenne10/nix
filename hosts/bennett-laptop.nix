5ba10e5d8b39d3a225798aa173650946b61a01e4
Bryan Bennett <Bryan.Bennett@protonmail.com>
2024-02-25 09:52:34 -0500

    U2F Yubikey support

--- hosts/bennett-laptop.nix
+++ hosts/bennett-laptop.nix
@@ -59,6 +59,10 @@ let dwl = (pkgs.callPackage ../derivations/dwl.nix { dwl-src = dwl-src; }); in
       dwl
       pinentry-bemenu
     ];
+    home.file.u2f_keys = {
+      target = ".config/Yubico/u2f_keys";
+      text = "bryan:6wdZOqyH9e2yqQZxcIoWoh2Ns+asTlhxBnzwGJvLP4c6MkCdM6jht2hQ8hwVWaxcv/1W43g0Ct1kJOVZKXruyg==,pvVwE1EtFLyuofbyWwX93grSfhDZa1JWPmkPybtfovzEFoqUCIPoeH0oJx+subMVLutVokwtKP6DgJP8PTce2w==,es256,+presence";
+    };
 
     gtk = {
       enable = true;
@@ -284,5 +288,12 @@ let dwl = (pkgs.callPackage ../derivations/dwl.nix { dwl-src = dwl-src; }); in
   };
 
   services.upower.enable = true;
+
+  security.pam.u2f = {
+    enable = true;
+    cue = true;
+  };
+
+  security.pam.services.swaylock = { };
 }
 
