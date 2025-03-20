0b2912a4fa56b97044d2201f32061fe134f1efa4
Bryan Bennett <Bryan.Bennett@proton.me>
2025-03-20 08:09:29 -0400

    Add some packages to work profile

--- lib/home/work.nix
+++ lib/home/work.nix
@@ -1,7 +1,20 @@
 {pkgs, ...}: 
 {
-  services.nix-daemon.enable = true;
-  home.packages = [
-    pkgs.jetbrains.idea-community-bin
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+    "slack"
   ];
+  home.packages = builtins.attrValues {
+    vpn_up = (pkgs.callPackage ../../derivations/vpn_up.nix {});
+
+    inherit (pkgs)
+      slack
+      thunderbird
+      ghidra
+      supersonic
+    ;
+
+    inherit (pkgs.jetbrains)
+      idea-community-bin
+    ;
+  };
 }
