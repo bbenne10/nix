90394fd546d1ebd03d61a0daff0d6139c5f73b2b
Bryan Bennett <Bryan.Bennett@proton.me>
2025-07-13 09:31:43 -0400

    Update pulseaudio option

--- lib/by_host/bennett-laptop.nix
+++ lib/by_host/bennett-laptop.nix
@@ -71,7 +71,7 @@
   hardware.bluetooth.enable = true;
   security.rtkit.enable = true;
 
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
