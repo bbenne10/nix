312a28474cfe4051c6649d8b66ef79d5854e5659
Bryan Bennett <Bryan.Bennett@proton.me>
2025-08-24 08:25:59 -0400

    Add mpv support

--- lib/home/graphical.nix
+++ lib/home/graphical.nix
@@ -231,6 +231,20 @@ in
     };
   };
 
+  programs.mpv = {
+    enable = true;
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          mpris
+        ];
+        mpv = pkgs.mpv-unwrapped.override { waylandSupport = true; };
+      }
+    );
+    config = {
+      profile = "high-quality";
+      ytdl-format = "bestvideo+bestaudio";
     };
   };
 }
