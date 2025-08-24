fa303f1dc753775f80801de644443221e4dae14f
Bryan Bennett <Bryan.Bennett@proton.me>
2025-08-24 08:25:35 -0400

    Remove kitty
    Alacritty has been fine

--- lib/home/graphical.nix
+++ lib/home/graphical.nix
@@ -231,30 +231,6 @@ in
     };
   };
 
-  programs.kitty = {
-    enable = false;
-    themeFile = "everforest_dark_hard";
-    font.name = "RecMonoSmCasual Nerd Font";
-    font.size = 12;
-    shellIntegration.enableZshIntegration = true;
-    keybindings = {
-      "f13>d" = "layout_action decrease_num_full_size_windows";
-      "f13>i" = "layout_action increase_num_full_size_windows";
-      "f13>/" = "layout_action mirror toggle";
-      "f13>PLUS" = "layout_action mirror bias 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75";
-      "f13>j" = "next_window";
-      "f13>k" = "previous_window";
-      "f13>ENTER" = "new_window_with_cwd";
-      "f13>J" = "next_tab";
-      "f13>K" = "prev_tab";
-    };
-    settings = {
-      scrollback_lines = 10000;
-      enable_audio_bell = "no";
-      window_padding_width = 12;
-      shell = "${pkgs.zsh}/bin/zsh";
-      enabled_layouts = "tall:bias=50;full_size=1;mirrored=false";
-      background_opacity = 0.9;
     };
   };
 }
